0f83873dbc66ffc930ec7c6713f5f09108ea732d
KashbullinRail <kashbullinrail@gmail.com>
2022-10-14 13:05:50 +0300

    koin работает

--- app/src/main/java/com/example/canvaspaint/App.kt
+++ app/src/main/java/com/example/canvaspaint/App.kt
@@ -1,7 +1,9 @@
 package com.example.canvaspaint
 
-import android.app.Application
 
+import android.app.Application
+import org.koin.android.ext.koin.androidContext
+import org.koin.core.context.startKoin
 
 class App : Application() {
     override fun onCreate() {
--- app/src/main/java/com/example/canvaspaint/appModules.kt
+++ app/src/main/java/com/example/canvaspaint/appModules.kt
@@ -1,6 +1,7 @@
 package com.example.canvaspaint
 
-
+import org.koin.androidx.viewmodel.dsl.viewModel
+import org.koin.dsl.module
 
 
 val module = module {
